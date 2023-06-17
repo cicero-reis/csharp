626299db618ddb547f5728d480b18f64c822fa92
cicero-reis <ciceroreis@outlook.com>
2023-06-17 00:14:06 -0300

    C# Store Enumeração e Composição

---
+++ Store/App/Entities/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    class Client
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public DateTime BirthDate { get; private set; }
+        public Client() { }
+
+        public Client(string name, string email, DateTime birthDate)
+        {
+            Name = name;
+            Email = email;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Name);
+            sb.Append(", (");
+            sb.Append(BirthDate.ToString("dd/MM/yyyy"));
+            sb.Append(") - ");
+            sb.Append(Email);
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ Store/App/Entities/OrderItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; private set; }
+        public double Price { get; private set; }
+
+        public Product Product { get; private set; }
+        public OrderItem() { }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal()
+        {
+            return Price * Quantity;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(Product.Name);
+            sb.Append(", $");
+            sb.Append(Price.ToString("F2", CultureInfo.InvariantCulture));
+            sb.Append(" Quantity: ");
+            sb.Append(Quantity);
+            sb.Append(", Subtotal: ");
+            sb.Append(SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+    }
+}
