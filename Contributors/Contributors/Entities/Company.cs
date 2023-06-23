ad036f1c5293a319bcaf8d67b41c3b90c023c3de
cicero-reis <ciceroreis@outlook.com>
2023-06-22 21:21:24 -0300

    C# Herança e Polimorfismo

---
+++ Contributors/Contributors/Entities/Company.cs
@@ -0,0 +1,24 @@
+﻿using System.Security;
+
+namespace Contributors.Entities
+{
+    class Company : TaxPayer
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public Company() { }
+
+        public Company(string name, double anualIncome, int numberOfEmployees)
+            : base(name, anualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double Tax()
+        {
+            return NumberOfEmployees > 10
+                    ? AnualIncome * 0.14
+                    : AnualIncome * 0.16;
+        }
+    }
+}
