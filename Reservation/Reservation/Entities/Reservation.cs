cb0c2676eacc9c72fc114497afa8cca7e56937a6
cicero-reis <ciceroreis@outlook.com>
2023-06-27 20:50:16 -0300

    C# Tratamento de excecões

---
+++ Reservation/Reservation/Entities/Reservation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Hotel.Entities.Exceptions;
+
+namespace Hotel.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        
+        public Reservation() { }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public double Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkin < now || checkout < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Room " + RoomNumber 
+                + ", check-in: " + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: " + CheckOut.ToString("dd/MM/yyyy")
+                + ", " + Duration() + " nights";
+        }
+
+
+    }
+}
