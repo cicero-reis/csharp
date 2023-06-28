ea598a09f3f50609cc03a31a1de4c33b4d7f387e
cicero-reis <ciceroreis@outlook.com>
2023-06-28 20:16:51 -0300

    C# Herança e Polimorfismo

---
+++ Company/App/App/Entities/Individual.cs
@@ -0,0 +1,24 @@
+﻿using App.Entities;
+
+namespace App.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; set; }
+
+        public Individual() { }
+
+        public Individual(string name, double anualIncome, double healthExpenditures)
+            : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+
+        public override double Tax()
+        {
+            return AnualIncome < 20000.0
+                ? (AnualIncome * 0.15 - HealthExpenditures * 0.5)
+                : (AnualIncome * 0.25 - HealthExpenditures * 0.5);
+        }
+    }
+}
