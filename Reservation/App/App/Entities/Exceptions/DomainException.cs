3489a6819d1207cf0d40936bf9a353695d5b08ee
cicero-reis <ciceroreis@outlook.com>
2023-06-28 20:27:22 -0300

    C# Tratamento de exceções

---
+++ Reservation/App/App/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace App.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message) { }
+
+    }
+}
