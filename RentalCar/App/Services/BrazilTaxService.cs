fb6af6cb8ac2d3c2d2da5ad99d90b48fdc3f7dca
cicero-reis <ciceroreis@outlook.com>
2023-07-08 13:07:07 -0300

    C# Interface

---
+++ RentalCar/App/Services/BrazilTaxService.cs
@@ -0,0 +1,12 @@
+﻿namespace App.Services
+{
+    class BrazilTaxService : ITaxService
+    {
+        public double Tax(double amount)
+        {
+            return amount <= 100.00
+                    ? amount * 0.2
+                    : amount * 0.15;
+        }
+    }
+}
---
+++ RentalCar/App/Services/RentalService.cs
@@ -0,0 +1,35 @@
+﻿using App.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        private ITaxService _taxService;
+
+        public RentalService(double pricePerHour, double pricePerDay, ITaxService taxService)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            _taxService = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = duration.TotalHours <= 12.0
+                ? PricePerHour * Math.Ceiling(duration.TotalHours)
+                : PricePerDay * Math.Ceiling(duration.TotalDays);
+
+            carRental.Invoice = new Invoice(basicPayment, _taxService.Tax(basicPayment));
+        }
+    }
+}
