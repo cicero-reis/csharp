981860393791500affeee8e16c0678300b106d91
cicero-reis <ciceroreis@outlook.com>
2023-07-14 21:57:50 -0300

    C# Generics

---
+++ Generics/App/Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace App.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is not Product)
+            {
+                throw new ArgumentException("Comparing error: argument is not an Product");
+            }
+
+            Product? other = obj as Product;
+
+            return Price.CompareTo(other?.Price);
+        }
+
+        public override string ToString()
+        {
+            return "Max: " 
+                    + Name 
+                    + ", " 
+                    + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Generics/App/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using App.Entities;
+using App.Services;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');
+
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+
+                list.Add(new Product(vect[0], price));
+            }
+
+            Product p = CalculationService.Max(list);
+
+            Console.WriteLine("Most expensive: ");
+
+            Console.WriteLine(p);
+        }
+    }
+}
